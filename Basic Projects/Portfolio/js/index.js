409ccadd661beb2bd4c4da9722bce4d7f087acfd
unknown <carl@specialistmetalworkskill.com>
2016-05-25 20:14:10 +0100

    Change image info slide effect

--- Basic Projects/Portfolio/js/index.js
+++ Basic Projects/Portfolio/js/index.js
@@ -1,17 +1,3 @@
 $(document).ready(function() {
 
-  $(".demo-box").hover(
-
-    function() {
-      var id = $(this).attr('id');
-    //alert(id);
-      $("#info-one").(".hovertext").animate({
-        "top": "-=140px"
-      }, "fast");
-    },
-    function() {
-      $("#info-one").(".hovertext").animate({
-        "top": "+=140px"
-      }, "fast");
-    });
 });
